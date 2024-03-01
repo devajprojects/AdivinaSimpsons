ff04072cc9a3d6006ddb2f30dcbc880fdec5a46d
Amuyen98 <amuyen.aquino@gmail.com>
2024-03-01 03:09:34 -0300

    arreglo de url api

--- models/simpsonsModel.js
+++ models/simpsonsModel.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 // Definir función para obtener un personaje aleatorio de Los Simpsons
 async function obtenerPersonajeAleatorio() {
     try {
-        const response = await axios.get('https://thesimpsonsquoteapi.glitch.me');
+        const response = await axios.get('https://thesimpsonsquoteapi.glitch.me/quotes');
         return response.data[0];
     } catch (error) {
         throw new Error('Error al obtener el personaje de Los Simpsons:', error.message);
